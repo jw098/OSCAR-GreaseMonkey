a1b50da4bef5bd8c7d500a417479b1152d7588d9
Justin Wong <justin.wong098@gmail.com>
2022-10-06 20:25:15 -0700

    eForms_KS: implemented keyboard shortcuts to submit and print/submit

--- eForms_KeyboardShortcuts.user.js
+++ eForms_KeyboardShortcuts.user.js
@@ -2,15 +2,22 @@
 // @name           eForms_KeyboardShortcuts
 // @namespace      oscar
 // @include        */eform/efmformslistadd.jsp*
+// @include        */eform/efmformadd_data.jsp*
 // @description		Within e-forms repository, Alt+A to close. Within an individual e-form: Alt+1 to Submit. Alt+2 to Print & Submit.
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // @grant	   none
 // ==/UserScript==
 
-const medicationHotkey = 'q';
-const consultationHotkey = 'w';
-const eFormsHotkey = 'a';
-const ticklerHotkey = 'z';
+const closeEFormList_hotkey = 'a';
+const submitEForm_hotkey = 1;
+const printNSubmitEForm_hotkey = 2;
 
+
+
+
+let currentURL = window.location.href;
+const eFormsPage = /eform\/efmformslistadd\.jsp/;
+const individualEForm = /eform\/efmformadd\_data\.jsp/;
 
 
 window.addEventListener('keydown', function(theEvent) {
@@ -23,19 +30,24 @@ window.addEventListener('keydown', function(theEvent) {
 	const theCtrlKey = theEvent.ctrlKey;
 	const theShiftKey= theEvent.shiftKey;
     
-	let currentURL = window.location.href;
-	const medPage = /oscarRx\/choosePatient\.do/
-	const eChartPage = /casemgmt\/forward\.jsp\?action\=view\&/;
-	const eFormsPage = /eform\/efmformslistadd\.jsp/;
-	const consultationPage = /oscarConsultationRequest\/ConsultationFormRequest\.jsp/;
-	const ticklerPage = /tickler\/ticklerAdd\.jsp/;
-	
+
 	
 	// console.log(currentURL);
 	// console.log(eChartPage.test(currentURL));
 	switch(true){
-		case eFormsPage.test(currentURL) && theAltKey && theKey == eFormsHotkey:	// If on eForms page, hotkey to close window.
+		case eFormsPage.test(currentURL) && theAltKey && theKey == closeEFormList_hotkey:	// If on eForms page, hotkey to close window.
 			window.close();
+			break;
+		case individualEForm.test(currentURL):
+			// let theTarget;
+			switch(true){
+				case theAltKey && theKey == submitEForm_hotkey:
+					$('#SubmitButton').click();
+					break;
+				case theAltKey && theKey == printNSubmitEForm_hotkey:
+					$('#PrintSubmitButton').click();
+					break;
+			}
 			break;
 	}
 }, true);
